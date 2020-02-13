b3ffecd02a82fc7705fbf54cb9667b7d5b4269c6
Spencer McDonough <shmcdono@usc.edu>
2020-02-12 22:22:38 -0800

    first couple fns q2

--- q2-max_heap/src/lib/solution.cc
+++ q2-max_heap/src/lib/solution.cc
@@ -1,2 +1,49 @@
 #include "solution.h"
 
+/******************** get index functions ********************/
+int MaxHeap::GetParentIndex(int i);
+{
+    int result = 0;
+
+    // input validation
+    if (i <= 0)
+    {
+        result = -1;
+    }
+    else
+    {
+        result = (i - 1) / 2;
+    }
+
+    return result;
+} 
+
+int MaxHeap::GetLeftIndex(int i)
+{
+    int result = 0;
+
+    // input validation
+    if((2 * i) + 1 >= (int)this->data_.size())
+    {
+        result = -1;
+    }
+    else
+    {
+        result = (2 * i) + 2;
+    }
+    return result;
+}
+int MaxHeap::GetRightIndex(int i); // *GT*
+int MaxHeap::GetSmallestChildIndex(int i); // *GT*
+
+// get value functions
+int MaxHeap::GetLeft(int i);
+int MaxHeap::GetRight(int i);
+int MaxHeap::GetParent(int i);
+
+// manipulate heap functions
+int MaxHeap::top(); // GT
+void MaxHeap::push(int v); // GT
+void MaxHeap::pop(); // GT
+void MaxHeap::TrickleUp(int i);
+void MaxHeap::TrickleDown(int i);
--- q2-max_heap/src/lib/solution.h
+++ q2-max_heap/src/lib/solution.h
@@ -3,6 +3,31 @@
 
 #include <vector>     // std::vector<int>
 
+// Only methods which are marked by "GT" should be tested
+class MaxHeap 
+{
+    private:
+        std::vector<int> data_;
+    public:
+        MaxHeap(); // default constructor
 
+        // get index functions
+        int GetParentIndex(int i); // *GT*
+        int GetLeftIndex(int i); // *GT*
+        int GetRightIndex(int i); // *GT*
+        int GetSmallestChildIndex(int i); // *GT*
+
+        // get value functions
+        int GetLeft(int i);
+        int GetRight(int i);
+        int GetParent(int i);
+
+        // manipulate heap functions
+        int top(); // GT
+        void push(int v); // GT
+        void pop(); // GT
+        void TrickleUp(int i);
+        void TrickleDown(int i);
+    };
 
 #endif
